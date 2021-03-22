07e76b832e62b12110f85d2d0cb3fb421fb22e50
Termonoid <termonoid@yandex.ru>
2021-03-22 22:30:30 +0300

    Rewrite backend to async

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,34 +1,33 @@
 use std::io::{self};
-use std::net::{TcpListener, TcpStream};
+use tokio::net::{TcpListener, TcpStream};
 
 use protocol::*;
 
 mod pivagen;
 
-fn handle_connection(mut connection: TcpStream, generator: &mut pivagen::Piva) -> io::Result<()> {
-    let request = Request::from_reader(&mut connection)?;
+async fn handle_connection(mut connection: TcpStream, generator: &mut pivagen::Piva) -> io::Result<()> {
+    let request = Request::receive(&mut connection).await?;
     if request.write_intent {
         generator.save_message(request.content)?;
     } else {
         let answer = generator.generate_answer(&request.content);
-        answer.to_writer(&mut connection)?;
+        answer.send(&mut connection).await?;
     }
     Ok(())
 }
 
-fn main() -> io::Result<()> {
+#[tokio::main(flavor="current_thread")]
+async fn main() -> io::Result<()> {
     let mut generator = pivagen::Piva::new()?;
-    let listener = TcpListener::bind("localhost:7482")?; // PIVA on T9 keyboard
-
-    for connection in listener.incoming() {
-        if let Ok(connection) = connection {
-            match handle_connection(connection, &mut generator) {
-                Ok(_) => {}
-                Err(e) => {
-                    println!("Connection lost: {}", e)
-                }
-            }
+    let listener = TcpListener::bind("localhost:7482").await?; // PIVA on T9 keyboard
+    
+    loop {
+        let connection = listener.accept().await;
+        if let Ok((connection, _)) = connection {
+            match handle_connection(connection, &mut generator).await {
+                Ok(_) =>  {},
+                Err(e) => {println!("Connection lost: {}", e)}
+            };
         }
     }
-    Ok(())
 }
