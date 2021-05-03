1d67fcb781d61dce84d664f9eeec66f5bf76d30d
Termonoid <termonoid@yandex.ru>
2021-05-03 13:47:12 +0300

    Async support in vk crate (#1)

--- frontends/vk/src/api.rs
+++ frontends/vk/src/api.rs
@@ -1,19 +1,20 @@
 use crate::TOKEN;
 use serde::Deserialize;
-use std::collections::VecDeque;
 use std::error::Error;
 use std::iter::Iterator;
 
 macro_rules! gen_vk_call {
     ($vis:vis $name:ident $method:literal $( : $( $argument:ident=$type:ty ),+ )? => $ret:ty) => {
-        $vis fn $name($($( $argument: $type ),+)?) -> Result<$ret, Box<dyn Error>> {
-            let response = ureq::post(concat!("https://api.vk.com/method/", $method))
-                .send_form(&[
+        $vis async fn $name($($( $argument: $type ),+)?) -> Result<$ret, Box<dyn Error>> {
+            let client = reqwest::Client::new();
+            let response = client.post(concat!("https://api.vk.com/method/", $method))
+                .form(&[
                     ("access_token", TOKEN),
                     ("v", "5.95"),
                $($( (stringify!($argument), &$argument.to_string()) ),+ )?
-                ])?;
-            let text = response.into_string()?;
+                ])
+                .send().await?;
+            let text = response.text().await?;
             if cfg!(debug_assertions) { println!("{}", &text) }
             let data: $ret = serde_json::from_str(&text)?;
             Ok(data)
@@ -49,23 +50,21 @@ pub struct Group {
 }
 #[derive(Deserialize)]
 pub struct LongPoll {
-    server: Box<str>,
-    key: Box<str>,
-    ts: Box<str>,
-    #[serde(skip)]
-    cache: VecDeque<Message>,
+    server: String,
+    key: String,
+    ts: String,
     #[serde(skip)]
     group_id: i32,
 }
 #[derive(Deserialize)]
 struct LongPollOk {
-    ts: Box<str>,
+    ts: String,
     updates: Vec<Object>,
 }
 #[derive(Deserialize)]
 struct LongPollError {
     failed: u8,
-    ts: Option<Box<str>>,
+    ts: Option<String>,
 }
 #[derive(Deserialize)]
 #[serde(untagged)]
@@ -74,69 +73,56 @@ enum LongPollResult {
     Error(LongPollError),
 }
 impl LongPoll {
-    pub fn new(id: i32) -> Result<LongPoll, Box<dyn Error>> {
-        let mut res = get_longpoll(id)?.response;
+    pub async fn new(id: i32) -> Result<LongPoll, Box<dyn Error>> {
+        let mut res = get_longpoll(id).await?.response;
         res.group_id = id;
         Ok(res)
     }
-    fn get_events(&mut self) -> Result<(), Box<dyn Error>> {
-        let response = ureq::post(&self.server).send_form(&[
+    pub async fn get_events(&mut self, client: &reqwest::Client) -> Result<Vec<Message>, Box<dyn Error>> {
+        let response = client.post(&self.server).form(&[
             ("act", "a_check"),
             ("key", &self.key),
             ("ts", &self.ts),
             ("wait", "25"),
-        ]);
-        if let Ok(response) = response {
-            let text = response.into_string()?;
-            if cfg!(debug_assertions) {
-                println!("{}", &text)
+        ]).send().await?;
+        let text = response.text().await?;
+        if cfg!(debug_assertions) {
+            println!("{}", &text)
+        }
+        match serde_json::from_str::<LongPollResult>(&text)? {
+            LongPollResult::Events(ok) => {
+                self.ts = ok.ts;
+                return Ok(ok.updates.into_iter().map(|o|o.object).collect())
             }
-            match serde_json::from_str::<LongPollResult>(&text)? {
-                LongPollResult::Events(ok) => {
-                    self.ts = ok.ts;
-                    self.cache
-                        .extend(ok.updates.into_iter().map(|obj| obj.object))
-                }
-                LongPollResult::Error(err) => {
-                    println!("got longpoll error {}", err.failed);
-                    let new_longpoll = Self::new(self.group_id)?;
-                    match err.failed {
-                        1 => self.ts = err.ts.unwrap(),
-                        2 => self.key = new_longpoll.key,
-                        3 => {
-                            self.key = new_longpoll.key;
-                            self.ts = new_longpoll.ts
-                        }
-                        _ => {
-                            *self = new_longpoll;
-                        }
+            LongPollResult::Error(err) => {
+                println!("got longpoll error {}", err.failed);
+                let new_longpoll = Self::new(self.group_id).await?;
+                match err.failed {
+                    1 => self.ts = err.ts.unwrap(),
+                    2 => self.key = new_longpoll.key,
+                    3 => {
+                        self.key = new_longpoll.key;
+                        self.ts = new_longpoll.ts;
+                    }
+                    _ => {
+                        *self = new_longpoll;
                     }
                 }
+                Ok(vec![])
             }
         }
-        Ok(())
     }
 }
-impl Iterator for LongPoll {
-    type Item = Message;
-    fn next(&mut self) -> Option<Self::Item> {
-        while self.cache.is_empty() {
-            self.get_events().unwrap_or_else(|_| {});
-        }
-        self.cache.pop_front()
-    }
-}
-
 impl Message {
-    pub fn reply(&self, text: &str) {
+    pub async fn reply(&self, text: &str, client: &reqwest::Client) {
         let random_id: i64 = rand::random();
-        let response = ureq::post("https://api.vk.com/method/messages.send").send_form(&[
+        let response = client.post("https://api.vk.com/method/messages.send").form(&[
             ("access_token", TOKEN),
             ("v", "5.95"),
             ("random_id", &random_id.to_string()),
             ("peer_id", &self.peer_id.to_string()),
             ("message", text),
-        ]);
+        ]).send().await;
         if response.is_err() {
             eprintln!("ERROR {}", response.unwrap_err())
         }
--- frontends/vk/src/main.rs
+++ frontends/vk/src/main.rs
@@ -13,31 +13,41 @@ fn should_reply(message: &Message, my_id: i32) -> bool {
             && message.reply_message.as_ref().unwrap().from_id == -my_id)
 }
 
-fn main() -> Result<(), Box<dyn Error>> {
+#[tokio::main(flavor = "current_thread")]
+async fn main() -> Result<(), Box<dyn Error>> {
     let mut rand = rand::thread_rng();
-    let group_id = get_me()?.response[0].id;
-    let longpoll = LongPoll::new(group_id)?;
+    let group_id = get_me().await?.response[0].id;
+    let client = reqwest::Client::new();
+    let mut longpoll = LongPoll::new(group_id).await?;
     let backend_unavailable = String::from("Ааеаоаооаа");
 
-    for m in longpoll {
-        if m.from_user() {
-            let res = client::save(&m.text);
-            if let Err(e) = res {
-                println!("Failed to save: {}", e);
+    loop {
+        let events = match longpoll.get_events(&client).await {
+            Ok(events) => events,
+            Err(e) => {eprintln!("{}", e);
+                       continue}
+        };
+        for m in events {
+            if m.from_user() {
+                let res = client::save(&m.text).await;
+                if let Err(e) = res {
+                    println!("Failed to save: {}", e);
+                }
             }
-        }
 
-        if should_reply(&m, group_id) {
-            let reply = match client::generate(&m.text) {
-                Ok(reply) => reply,
-                Err(_) => backend_unavailable.clone(),
-            };
-            m.reply(&reply)
-        } else if rand.gen_bool(0.05) {
-            if let Ok(reply) = client::generate(&m.text) {
-                m.reply(&reply)
+            if should_reply(&m, group_id) {
+                let reply = match client::generate(&m.text).await {
+                    Ok(reply) => reply,
+                    Err(e) => {
+                        eprintln!("{}", e);
+                        backend_unavailable.clone()},
+                };
+                m.reply(&reply, &client).await
+            } else if rand.gen_bool(0.05) {
+                if let Ok(reply) = client::generate(&m.text).await {
+                    m.reply(&reply, &client).await
+                }
             }
         }
     }
-    Ok(())
 }
