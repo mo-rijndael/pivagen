780e85592fe8ec59697ac8b60ee0a98b78a0aad5
Termonoid <termonoid@yandex.ru>
2021-08-08 13:29:34 +0300

    frontends/vk: fix metrics

--- frontends/vk/src/metrics.rs
+++ frontends/vk/src/metrics.rs
@@ -1,29 +1,21 @@
+use postgres_derive::ToSql;
 use tokio_postgres::{Client, Error};
 
-#[derive(Clone, Copy)]
+#[derive(Debug, Clone, Copy, ToSql)]
+#[postgres(name = "chat_type")]
 pub enum Chat {
+    #[postgres(name = "private")]
     Private,
+    #[postgres(name = "group")]
     Group,
 }
-impl Chat {
-    fn as_str(&self) -> &'static str {
-        match self {
-            Self::Private => "private",
-            Self::Group => "group",
-        }
-    }
-}
+#[derive(Debug, ToSql)]
+#[postgres(name = "message_type")]
 pub enum MsgType {
+    #[postgres(name = "incoming")]
     Incoming,
+    #[postgres(name = "outgoing")]
     Outgoing,
-}
-impl MsgType {
-    fn as_str(&self) -> &'static str {
-        match self {
-            Self::Incoming => "incoming",
-            Self::Outgoing => "outgoing",
-        }
-    }
 }
 pub async fn write_message(
     client: &Client,
@@ -32,8 +24,8 @@ pub async fn write_message(
 ) -> Result<(), Error> {
     client
         .execute(
-            "INSERT INTO pivagen_vk VALUES (NOW(), '$1', '$2')",
-            &[&msg_type.as_str(), &chat_type.as_str()],
+            "INSERT INTO pivagen_vk VALUES (NOW(), $1, $2)",
+            &[&chat_type, &msg_type],
         )
         .await?;
     Ok(())
